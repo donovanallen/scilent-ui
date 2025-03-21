b5889c10d53f5756120f30694aa2e36c7f97aa39
Donovan Allen <dallen.928@gmail.com>
2025-03-21 12:26:52 -0400

    style: landing page sections

---
+++ apps/showcase/src/Terminal.tsx
@@ -0,0 +1,23 @@
+interface TerminalProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export const Terminal = ({ children, className }: TerminalProps) => {
+  return (
+    <div
+      className={`z-0 h-full max-h-[400px] w-full rounded-xl border border-border bg-background ${className}`}
+    >
+      <div className="flex flex-col gap-y-2 border-b border-border p-4">
+        <div className="flex flex-row gap-x-2">
+          <div className="h-2 w-2 rounded-full bg-red-500"></div>
+          <div className="h-2 w-2 rounded-full bg-yellow-500"></div>
+          <div className="h-2 w-2 rounded-full bg-green-500"></div>
+        </div>
+      </div>
+      <pre className="p-4">
+        <code className="grid gap-y-1 overflow-auto">{children}</code>
+      </pre>
+    </div>
+  );
+};
